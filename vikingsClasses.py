5c9602b470635153b0e6bb40c65694641df997ab
Ëduardo Cabrera <edumcabrera@gmail.com>
2025-03-16 13:37:37 +0100

    file modified

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -6,36 +6,52 @@ import random
 class Soldier:
     def __init__(self, health, strength):
         # your code here
+        self.health = health
+        self.strength = strength
     
     def attack(self):
-        # your code here
-
-    def receiveDamage(self, damage):
-        # your code here
+        return self.strength
     
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
         # your code here
+        super().__init__(health, strength)
+        self.name = name
 
     def battleCry(self):
         # your code here
+        return "Odin Owns You All!"
 
     def receiveDamage(self, damage):
         # your code here
-
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage."
+        else:
+            return f"{self.name} has died in act of combat."
+        
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
         # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
         # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"."
+        else:
+            return f"A Saxon has died in combat."
 
-# Davicente
+# WAAAAAAAAAGH
 
 class War():
     def __init__(self):
@@ -56,28 +72,3 @@ class War():
     def showStatus(self):
         # your code here
     pass
-
-#yop
-class War2:
-
-    def __init__(self):
-        # your code here
-
-    def addViking(self, Viking):
-        # your code here
-    
-    def addSaxon(self, Saxon):
-        # your code here
-    
-    def vikingAttack(self):
-        # your code here
-
-    def saxonAttack(self):
-        # your code here
-
-    def showStatus(self):
-        # your code here
-
-    pass
-
-
